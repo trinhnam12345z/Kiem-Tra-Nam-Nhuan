16075ea8ca3012d3607b25ae20103ba3a9f5359b
trinhnam12345z <trinhnam123456789z@gmail.com>
2020-09-08 16:07:25 +0700

    Kiểm tra năm nhuận

---
+++ ktNamNhuan.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace KiemTraNamNhuan
+{
+    class ktNamNhuan
+    {
+        static void Main(string[] args)
+        {
+            int year;
+            Console.WriteLine("CT kiem tra nam nhuan !");
+            Console.Write("Nhap nam : ");
+            year = int.Parse(Console.ReadLine());
+            if((year % 400) == 0)
+            {
+                Console.WriteLine("{0} la nam nhuan ", year);
+            }else if ((year % 100) == 0)
+            {
+                Console.WriteLine("{0} khong phai la nam nhuan", year);
+            }else if ((year % 4 ) == 0)
+            {
+                Console.WriteLine("{0} la nam nhuan", year);
+            }else
+            {
+                Console.WriteLine("{0} khong phai la nam nhuan", year);
+            }
+            Console.ReadKey();
+        }
+    }
+}
